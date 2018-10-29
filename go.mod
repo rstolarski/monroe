bd9f43591d9d31578fd3d7ada0cfdcb7ef23a8b9
Ryszard T <ryszard.tropisz@gmail.com>
2018-10-29 22:42:25 +0100

    [+] Organized images into resources directory [*] Using different method to generate each frame

--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module github.com/rtropisz/monroe
 
 require (
 	github.com/alok87/goutils v0.0.0-20170710173822-29ef704e5505
+	github.com/andybons/gogif v0.0.0-20140526152223-16d573594812
+	github.com/anthonynsimon/bild v0.10.0
 	github.com/disintegration/gift v1.2.0
 	github.com/disintegration/imaging v1.5.0
 	golang.org/x/image v0.0.0-20180926015637-991ec62608f3 // indirect
--- main.go
+++ main.go
@@ -1,16 +1,84 @@
 package main
 
 import (
-	"github.com/rtropisz/monroe/monroe"
+	"fmt"
+    "math/rand"
+    "time"
+	"image/png"
+	"os"
+	"image"
+	"log"
+
+	"github.com/anthonynsimon/bild/blend"
+	"github.com/anthonynsimon/bild/imgio"
+	"github.com/anthonynsimon/bild/transform"
+	//"github.com/andybons/gogif"
 )
 
-func main() {
-	m := monroe.ReadMonroe("input")
-	m.SplitMonroe()
-	m.ShiftImages()
+//"github.com/rtropisz/monroe/monroe"
+func random(min, max int) int {
+	return min + rand.Intn(max-min)
+}
 
-	m.CombineMonroe()
-	m.MaskCMYK()
-	//m.OutputMultipleMonroe("output")
-	m.OutputMonroe(m.GetOutputImage(), "output")
+const inputPath = "resources/CMYK_split/"
+const outPath = "img/"
+const i = 5
+
+func main() {
+
+	C := read(inputPath+"cyan")
+	Y := read(inputPath+"yellow")
+	M := read(inputPath+"magenta")
+	//K := read(inputPath+"black")
+
+	rand.Seed(time.Now().UTC().UnixNano())
+	x := random(-i,i)
+	y := random(-i,i)
+	C = transform.Translate(C,x,y)
+	x = random(-i,i)
+	y = random(-i,i)
+	Y = transform.Translate(Y,x,y)
+	x = random(-i,i)
+	y = random(-i,i)
+	M = transform.Translate(M,x,y)
+	
+	// Don't move Key
+	// K = transform.Translate(K,-5,6)
+
+	CM := blend.Multiply(C, M)
+	CMY := blend.Multiply(CM, Y)
+	//CMYK := blend.Multiply(CMY, K)
+
+	save("C", C)
+	save("CM", CM)
+	save("CMY", CMY)
+	//save("CMYK", CMYK)
+}
+
+func read(input string) image.Image {
+	img, err := imgio.Open(input + ".png")
+	if err != nil {
+		log.Fatalf("failed to open image: %v", err)
+	}
+	return img
+}
+
+func save(output string, image image.Image) {
+	err := os.MkdirAll(outPath, 0777)
+    if err != nil {
+    	fmt.Errorf("MkdirAll %q: %s", outPath, err)
+    }
+	f, err := os.Create(outPath + output + ".png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := png.Encode(f, image); err != nil {
+		f.Close()
+		log.Fatal(err)
+	}
+
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
 }

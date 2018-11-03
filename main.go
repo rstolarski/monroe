8fb5c7147a8202f8f08f07986cbec8bc61c84f93
Ryszard T <ryszard.tropisz@gmail.com>
2018-11-03 20:59:34 +0100

    [+] Generating each frame as an image [*] Resizing images depending on main constant

--- main.go
+++ main.go
@@ -1,21 +1,22 @@
 package main
 
 import (
-	"image/color"
 	"fmt"
-    "math/rand"
-    "time"
-	"image/png"
-	"os"
 	"image"
+	"image/color"
+	"image/png"
 	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/disintegration/imaging"
 
 	"github.com/anthonynsimon/bild/blend"
 	"github.com/anthonynsimon/bild/imgio"
 	"github.com/anthonynsimon/bild/transform"
-	"github.com/anthonynsimon/bild/noise"
-	"github.com/anthonynsimon/bild/effect"
-	//"github.com/andybons/gogif"
 )
 
 //"github.com/rtropisz/monroe/monroe"
@@ -25,81 +26,107 @@ func random(min, max int) int {
 
 const inputPath = "resources/CMYK_split/"
 const outPath = "img/"
-const shift = 10
-// Should be 3-5
-const noiseSize = 3
+const outShift = 3
 
 func main() {
+	C := read(inputPath + "cyan")
+	Y := read(inputPath + "yellow")
+	M := read(inputPath + "magenta")
+	K := read(inputPath + "black")
 
-	C := read(inputPath+"cyan")
-	Y := read(inputPath+"yellow")
-	M := read(inputPath+"magenta")
-	mask := read(inputPath+"mask_temp")
-	K := read(inputPath+"black")
-	noise := effect.Invert(makeSomeNoise(C.Bounds().Max.X))
-	noiseAsAlpha := rgbaToAlpha(blend.Subtract(effect.Invert(mask), noise))
-	save("new_mask", noiseAsAlpha)
+	frames := make([]*image.NRGBA, outShift*outShift*4)
 
-	fC := toRGBA(C)
-	fM := toRGBA(M)
-	fY := toRGBA(Y)
-
-	r := rand.Intn(3) 
-	switch r {
-	case 0:
-		fmt.Printf("Cyan\n")
-		// fC = maskuj(fC, noiseAsAlpha)
-		fC = maskRGBA(fC, noiseAsAlpha)
-		save("fC",fC)
-	case 1:
-		fmt.Printf("Magenta\n")
-		fM = maskRGBA(fM, noiseAsAlpha)
-		save("fM",fM)
-	case 2:
-		fmt.Printf("Yellow\n")
-		fY = maskRGBA(fY, noiseAsAlpha)
-		save("fY",fY)
-	}
-
-	rand.Seed(time.Now().UTC().UnixNano())
-	r = rand.Intn(3) 
-
-	switch r {
-	case 0:
-		fC = shiftImage(fC)
-	case 1:
-		fM = shiftImage(fM)
-	case 2:
-		fY = shiftImage(fY)
-	}
-
-	C = blend.Multiply(K, fC)
-	CM := blend.Multiply(C, fM)
-	CMY := blend.Multiply(CM, fY)
-	
-
-	save("CK", C)
-	save("CMK", CM)
-	save("CMYK", CMY)
-
-}
-
-func rgbaToAlpha(img *image.RGBA) *image.Alpha {
-	out := image.NewAlpha(img.Bounds())
-	for y := 0; y < img.Bounds().Max.Y; y++ {
-		for x := 0; x < img.Bounds().Max.X; x++ {
-			oldColor := img.RGBAAt(x, y)
-            color := color.Alpha{oldColor.G}
-			out.Set(x,y,color)
+	ptX := C.Bounds().Max.X
+	ptY := C.Bounds().Max.Y
+	p := image.Point{ptX / outShift, ptY / outShift}
+	dst := imaging.New(ptX, ptY, color.RGBA{0, 0, 0, 0})
+	for x := 0; x < outShift; x++ {
+		for y := 0; y < outShift; y++ {
+			dstK, dstC, dstCM, dstCMY := convertAndShiftAllImages(K, C, M, Y, ptX/outShift, (5/outShift)*2)
+			newP := image.Point{p.X * x, p.Y * y}
+			dst = combineAllImages(frames, dst, dstK, dstC, dstCM, dstCMY, newP)
 		}
 	}
-	return out
+	log.Printf("Number of frames: %v\n", len(frames))
 }
 
-func shiftImage(src *image.RGBA) *image.RGBA {
-	x := random(-shift,shift)
-	y := random(-shift,shift)
-	return transform.Translate(src,x,y)
+func combineAllImages(
+	f []*image.NRGBA,
+	dst *image.NRGBA,
+	dstK *image.RGBA,
+	dstC *image.RGBA,
+	dstCM *image.RGBA,
+	dstCMY *image.RGBA,
+	p image.Point) *image.NRGBA {
+
+	dst = imaging.Paste(dst, dstK, p)
+	f = append(f, dst)
+	save(strconv.Itoa(p.X)+"_"+strconv.Itoa(p.Y)+"_0", dst)
+	dst = imaging.Paste(dst, dstC, p)
+	f = append(f, dst)
+	save(strconv.Itoa(p.X)+"_"+strconv.Itoa(p.Y)+"_1", dst)
+	dst = imaging.Paste(dst, dstCM, p)
+	f = append(f, dst)
+	save(strconv.Itoa(p.X)+"_"+strconv.Itoa(p.Y)+"_2", dst)
+	dst = imaging.Paste(dst, dstCMY, p)
+	f = append(f, dst)
+	save(strconv.Itoa(p.X)+"_"+strconv.Itoa(p.Y)+"_3", dst)
+	return dst
+}
+
+func convertAndShiftAllImages(
+	K image.Image,
+	C image.Image,
+	M image.Image,
+	Y image.Image,
+	s, o int) (*image.RGBA, *image.RGBA, *image.RGBA, *image.RGBA) {
+
+	C = imaging.Resize(C, s, 0, imaging.Lanczos)
+	Y = imaging.Resize(Y, s, 0, imaging.Lanczos)
+	M = imaging.Resize(M, s, 0, imaging.Lanczos)
+	K = imaging.Resize(K, s, 0, imaging.Lanczos)
+
+	fK := toRGBA(K)
+	fC := toRGBA(C)
+	fY := toRGBA(Y)
+	fM := toRGBA(M)
+
+	rand.Seed(time.Now().UTC().UnixNano())
+	r := rand.Intn(3)
+	switch r {
+	case 0:
+		fC = shiftImage(fC, "cyan", o)
+	case 1:
+		fM = shiftImage(fM, "magenta", o)
+	case 2:
+		fY = shiftImage(fY, "yellow", o)
+	}
+
+	dstK := fK
+	dstC := blend.Multiply(fK, fC)
+	dstCM := blend.Multiply(dstC, fM)
+	dstCMY := blend.Multiply(dstCM, fY)
+
+	return dstK, dstC, dstCM, dstCMY
+}
+
+// func rgbaToAlpha(img *image.RGBA) *image.Alpha {
+// 	out := image.NewAlpha(img.Bounds())
+// 	for y := 0; y < img.Bounds().Max.Y; y++ {
+// 		for x := 0; x < img.Bounds().Max.X; x++ {
+// 			oldColor := img.RGBAAt(x, y)
+// 			color := color.Alpha{oldColor.G}
+// 			out.Set(x, y, color)
+// 		}
+// 	}
+// 	return out
+// }
+
+func shiftImage(src *image.RGBA, l string, o int) *image.RGBA {
+	x := random(-o, o)
+	y := random(-o, o)
+	log.Printf("%v, shifted by: %v, %v\n", strings.ToUpper(l), x, y)
+	return transform.Translate(src, x, y)
 }
 
 func toRGBA(img image.Image) *image.RGBA {
@@ -107,8 +134,8 @@ func toRGBA(img image.Image) *image.RGBA {
 	for y := 0; y < img.Bounds().Max.Y; y++ {
 		for x := 0; x < img.Bounds().Max.X; x++ {
 			oldColor := img.At(x, y)
-            color := color.RGBAModel.Convert(oldColor)
-			out.Set(x,y,color)
+			color := color.RGBAModel.Convert(oldColor)
+			out.Set(x, y, color)
 		}
 	}
 	return out
@@ -117,41 +144,27 @@ func toRGBA(img image.Image) *image.RGBA {
 func invert(img *image.Alpha) *image.Alpha {
 	for y := 0; y < img.Bounds().Max.Y; y++ {
 		for x := 0; x < img.Bounds().Max.X; x++ {
-			img.SetAlpha(x,y,color.Alpha{255-img.AlphaAt(x,y).A})
-		}
-	}
-	return img
-} 
-
-func makeSomeNoise(size int) *image.RGBA {
-	noise := noise.Generate(
-		noiseSize,
-		noiseSize,
-		&noise.Options{NoiseFn: noise.Uniform, Monochrome: true})
-	noise = transform.Resize(
-		noise,
-		size,
-		size,
-		transform.Lanczos,
-	)
-	return noise
-}
-
-func maskRGBA(img *image.RGBA, mask *image.Alpha) *image.RGBA {
-	for y := 0; y < img.Bounds().Max.Y; y++ {
-		for x := 0; x < img.Bounds().Max.X; x++ {
-			alpha := mask.AlphaAt(x,y).A
-			color := color.NRGBA{
-				img.RGBAAt(x,y).R,
-				img.RGBAAt(x,y).G,
-				img.RGBAAt(x,y).B,
-				alpha,
-			}
-			img.Set(x,y,color)
+			img.SetAlpha(x, y, color.Alpha{255 - img.AlphaAt(x, y).A})
 		}
 	}
 	return img
 }
+
+// func maskRGBA(img *image.RGBA, mask *image.Alpha) *image.RGBA {
+// 	for y := 0; y < img.Bounds().Max.Y; y++ {
+// 		for x := 0; x < img.Bounds().Max.X; x++ {
+// 			alpha := mask.AlphaAt(x, y).A
+// 			color := color.NRGBA{
+// 				img.RGBAAt(x, y).R,
+// 				img.RGBAAt(x, y).G,
+// 				img.RGBAAt(x, y).B,
+// 				alpha,
+// 			}
+// 			img.Set(x, y, color)
+// 		}
+// 	}
+// 	return img
+// }
 
 func read(input string) image.Image {
 	img, err := imgio.Open(input + ".png")
@@ -163,9 +176,9 @@ func read(input string) image.Image {
 
 func save(output string, image image.Image) {
 	err := os.MkdirAll(outPath, 0777)
-    if err != nil {
-    	fmt.Errorf("MkdirAll %q: %s", outPath, err)
-    }
+	if err != nil {
+		fmt.Errorf("MkdirAll %q: %s", outPath, err)
+	}
 	f, err := os.Create(outPath + output + ".png")
 	if err != nil {
 		log.Fatal(err)
@@ -179,4 +192,5 @@ func save(output string, image image.Image) {
 	if err := f.Close(); err != nil {
 		log.Fatal(err)
 	}
+	log.Printf("Image was saved\n")
 }
